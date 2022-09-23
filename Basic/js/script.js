2e202d85c42f87a6d3e46b318389d3eaa7741194
Arit-Su <aritrasur01@outlook.com>
2022-09-23 19:54:47 +0530

    first commit

---
+++ Basic/js/script.js
@@ -0,0 +1,30 @@
+/*var x;
+x = prompt("Enter your name ::");
+//document.write(x);
+console.log(x);
+*/
+
+/*
+let data = 100;
+function f1() {
+    document.write(data);
+}
+f1();
+f1();
+*/
+
+/*
+var a = 5;
+var b = 7;
+
+var c,d;
+
+c = a++;
+d = ++b;
+
+console.log(c);
+console.log(a);
+console.log(d);
+console.log(b);
+*/
+
